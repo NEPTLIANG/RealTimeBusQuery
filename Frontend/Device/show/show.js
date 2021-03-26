17c9a9ffdbe9608e7e136d1142527f9191173124
LM.Liang <lm.liang@rc.com>
2021-03-26 17:25:23 +0800

    feat(device): í±´完成设备状态设置前端功能；修改样式
    	1. 完成设备状态设置前端功能；
    	2. 修改样式
    
    Signed-off-by: LM.Liang <lm.liang@rc.com>

--- Frontend/Device/show/show.js
+++ Frontend/Device/show/show.js
@@ -61,13 +61,61 @@ function show(item) {
     var position = (item.lng && item.lat) ? (item.lng + "<br/>" + item.lat) : "暂无定位"
     var intro = item.intro ? item.intro : "暂无说明"
     card.className = "card"
-    card.innerHTML = `<h2>${item.name}</h2>
+    let { status } = item
+    card.innerHTML = `
+        <h2>${item.name}</h2>
         <span class="id">id: ${item.id}</span>
-        <div class="position"><div style="font-weight: bold">位置</div>${position}</div>
+        <div class="position">
+            <div style="font-weight: bold">位置</div>
+            ${status ? position : '设备已禁用'}
+        </div>
         <div>${intro}</div>
-        <a href='../modify/modify.html?id=${item.id}&name=${item.name}&route=${item.route}&intro=${item.intro}' class="edit">编辑</a>
-        <button onclick="del('${item.id}')" class="del">删除</button>`
+        <button onclick="switchStatus('${item.id}', '${status}')" class="cardButton" 
+            style="background-color: ${status ? '#f77' : '#777'};
+            color: ${status? '#fff' : '#eee'};
+            line-height: unset;
+        ">
+            ${status? '点击<br/>停用' : '点击<br/>启用'}
+        </button>
+        <a href='../modify/modify.html?id=${item.id}&name=${item.name}&route=${item.route}&intro=${item.intro}' class="cardOption">编辑</a>
+        <button onclick="del('${item.id}')" class="cardOption">删除</button>
+    `
     document.getElementById("list").appendChild(card)
+}
+
+function switchStatus(id, status) {
+    status = Number.parseInt(status)
+    if (!confirm(`确定要${status ? '停用' : '启用'}设备吗？`)) {
+        return
+    }
+    console.log('id=car1&status=0' === `id=${id}&status=${status ? 0 : 1}`)
+    let data = `id=${id}&status=${status ? 0 : 1}`
+    fetch('http://122.51.3.35/device.php', {
+        method: 'PUT',
+        body: data
+    }).then((response) => {
+        console.log(response)
+        try {
+            return response = response.json()
+        } catch (e) {
+            console.log(e)
+            alert("响应格式错误，请稍后重试")
+        }
+    }).then((response) => {
+        console.log(response)
+        if (response) {
+            if (response.status == 200) {
+                alert("修改成功")
+                loadData()
+            } else {
+                alert(response.message)
+            }
+        } else {
+            // alert("响应格式错误，请稍后重试")
+        }
+    }).catch((error) => {
+        alert(`${error}：出现错误，请稍后重试`)
+    })
 }
 
 var del = (id) => {
