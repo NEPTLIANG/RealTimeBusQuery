ad1ab65f94c790b67748c8a874b20c5617debbfc
LM.Liang <lm.liang@rc.com>
2021-04-01 18:40:35 +0800

    style: Backup

---
+++ Frontend/User/modifyInfo/modifyInfo.js
@@ -0,0 +1,84 @@
+/*
+ * @Author: NeptLiang
+ * @Date: 2021-04-01 09:30:59
+ * @LastEditors: NeptLiang
+ * @LastEditTime: 2021-04-01 14:39:54
+ * @Description: 修改信息/密码
+ */
+onload = () => {
+    var name = document.getElementById("name").value; //用户名
+    let changePwdCheckBox = document.getElementById('changePwd');
+    let pwdBox = document.getElementById('pwdBox');
+    changePwdCheckBox.addEventListener('click', (e) => {
+        pwdBox.style.display = e.target.checked ? 'block' : 'none';
+    })
+    var modifyBtn = document.getElementById("modify");
+    let changePwd = changePwdCheckBox.checked;
+    modifyBtn.addEventListener("click", function() {
+        if (changePwd) {
+            var oldPwd = CryptoJS.SHA512(document.getElementById("oldPwd").value).toString();
+            var newPwd = CryptoJS.SHA512(document.getElementById("newPwd").value).toString();
+            var confirmPwd = CryptoJS.SHA512(document.getElementById("confirmPwd").value).toString();
+            if (oldPwd !== confirmPwd) {
+                alert("两次密码不一致");
+                document.getElementById("newPwd").value = "";
+                document.getElementById("confirmPwd").value = "";
+                return;
+            }
+            var url = "http://122.51.3.35/user.php"
+            content = `name=${name}&oldPwd=${oldPwd}&newPwd=${newPwd}`;
+            if (typeof "XMLHttpRequest" !== "undefined") {
+                var xhr = new XMLHttpRequest();
+                xhr.onreadystatechange = () => {
+                    if (xhr.readyState == 4) {
+                        if ((xhr.status >= 200 && xhr.status < 300) || xhr.status === 304) {
+                            handleResponse(xhr)
+                        } else {
+                            alert("请求失败，请稍后再试");
+                        }
+                    }
+                }
+                xhr.open("PUT", url, true);
+                xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+                xhr.send(content);
+            }
+        } else {
+            fetch('http://122.51.3.35/user.php', {
+                method: 'PUT',
+                body: `name=${name}`
+            }).then(response =>
+                response.json()
+            ).then(response => {
+                if (response.status === 200) {
+                    alert("修改成功，请重新登录")
+                    location = "../login/login.html"
+                } else {
+                    alert(`修改失败：${response.message}`);
+                }
+            }).catch((e) => {
+                alert(`请求失败：${e.message}`);
+            })
+        }
+    })
+}
+
+/**
+ * 处理响应
+ * @param {object} xhr XMLHttpRequest响应对象
+ */
+function handleResponse(xhr) {
+    try {
+        console.log(xhr.responseText)
+        var response = JSON.parse(xhr.responseText);
+    } catch (e) {
+        alert("响应格式错误，请稍后再试");
+    }
+    if (typeof(response) !== "undefined") {
+        if (response.status === 200) {
+            alert("修改成功，请重新登录")
+            location = "../login/login.html"
+        } else {
+            alert(`修改失败：${response.message}`);
+        }
+    }
+}
